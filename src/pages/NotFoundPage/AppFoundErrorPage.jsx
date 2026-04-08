cc0b3a1dd9ccdedd2c8e866f16ba5b1ba58858e7
Pial <pialname394@gmail.com>
2026-04-09 02:45:05 +0600

    added responsive to the error page

--- src/pages/NotFoundPage/AppFoundErrorPage.jsx
+++ src/pages/NotFoundPage/AppFoundErrorPage.jsx
@@ -4,11 +4,13 @@ import errorImg from "../../assets/images/App-Error.png";
 
 const AppFoundErrorPage = () => {
   return (
-    <div className="container mx-auto py-20">
+    <div className="container mx-auto py-20 px-4 lg:px-0">
       <img src={errorImg} alt="Error" className="mx-auto mb-7" />
-      <div className="flex justify-center items-center flex-col space-y-5">
-        <h2 className="font-semibold text-5xl">OPPS!! APP NOT FOUND</h2>
-        <p className="text-xl text-gray-600">
+      <div className="flex justify-center items-center flex-col space-y-5 text-center">
+        <h2 className="font-semibold text-3xl md:text-5xl">
+          OPPS!! APP NOT FOUND
+        </h2>
+        <p className="text-md md:text-xl text-gray-600">
           The App you are requesting is not found on our system. please try
           another apps
         </p>
