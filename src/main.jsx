5d011d368fbfa34285394c64f2ab62d1a7de73c4
Pial <pialname394@gmail.com>
2026-04-08 21:44:40 +0600

    refactor main.jsx components

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter } from "react-router";
+import { router } from "./routes/Routes";
 import { RouterProvider } from "react-router/dom";
-import RootLayout from "./layout/RootLayout";
-import HomePage from "./pages/homepage/HomePage";
-import Apps from "./pages/apps/Apps";
-import Installation from "./pages/installation/Installation";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: RootLayout,
-    children: [
-      {
-        index: true,
-        Component: HomePage,
-      },
-      {
-        path: "/apps",
-        Component: Apps,
-      },
-      {
-        path: "/install",
-        Component: Installation,
-      },
-      {
-        path: "*",
-        Component: NotFoundPage,
-      },
-    ],
-  },
-]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
