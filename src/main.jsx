bfe07d786712f67713af481295bb349a55fa75db
Pial <pialname394@gmail.com>
2026-04-08 12:28:06 +0600

    create routing form react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,36 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from "./layout/RootLayout";
+import HomePage from "./pages/homepage/HomePage";
+import Apps from "./pages/apps/Apps";
+import Installation from "./pages/installation/Installation";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: HomePage,
+      },
+      {
+        path: "/apps",
+        Component: Apps,
+      },
+      {
+        path: "/install",
+        Component: Installation,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
