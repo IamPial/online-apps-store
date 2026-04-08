878f38ee22104b44d99ff00e77ec6bafce0b3e1f
Pial <pialname394@gmail.com>
2026-04-08 21:45:36 +0600

    create routes components for routes

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layout/RootLayout";
+import HomePage from "../pages/homepage/HomePage";
+import Apps from "../pages/apps/Apps";
+import Installation from "../pages/installation/Installation";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: HomePage,
+      },
+      {
+        path: "/apps",
+        Component: Apps,
+      },
+      {
+        path: "/install",
+        Component: Installation,
+      },
+      {
+        path: "*",
+        Component: NotFoundPage,
+      },
+    ],
+  },
+]);
