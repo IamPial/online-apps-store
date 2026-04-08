a7ef07bbb28bb25f2c2fc5c9c377c16b8652e159
Pial <pialname394@gmail.com>
2026-04-09 02:47:43 +0600

    added more responsiveness to the apps section

--- src/pages/apps/Apps.jsx
+++ src/pages/apps/Apps.jsx
@@ -56,7 +56,7 @@ const Apps = () => {
           {errorMessage ? (
             <AppFoundErrorPage />
           ) : (
-            <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 px-4 md:px-0">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-4 md:px-0">
               {allData.map((cardData, index) => {
                 return <Card key={index} cardData={cardData}></Card>;
               })}
