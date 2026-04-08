1ea01ebcaaa5c18087e3d42f3de5097baffd0325
Pial <pialname394@gmail.com>
2026-04-08 15:33:19 +0600

    added active styles to the navlink

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -35,33 +35,99 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <NavLink to="/">Home</NavLink>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    `font-medium text-[16px]  ${
+                      isActive
+                        ? "bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent  rounded-none "
+                        : "text-slate-950"
+                    }`
+                  }
+                >
+                  Home
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/apps">Apps</NavLink>
+                <NavLink
+                  to="/apps"
+                  className={({ isActive }) =>
+                    `font-medium text-[16px]  ${
+                      isActive
+                        ? "bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent  rounded-none "
+                        : "text-slate-950"
+                    }`
+                  }
+                >
+                  Apps
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/install">Installation</NavLink>
+                <NavLink
+                  to="/install"
+                  className={({ isActive }) =>
+                    `font-medium text-[16px]  ${
+                      isActive
+                        ? "bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent  rounded-none "
+                        : "text-slate-950"
+                    }`
+                  }
+                >
+                  Installation
+                </NavLink>
               </li>
             </ul>
           </div>
           <div className="flex items-center gap-2">
             <img src={logoImg} alt="Hero" className="w-10" />
-            <p className="font-bold bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-xl text-transparent uppercase">
+            <h2 className="font-bold bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-xl text-transparent uppercase">
               Hero.io
-            </p>
+            </h2>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
+          <ul className="menu menu-horizontal px-1 space-x-8">
             <li>
-              <NavLink to="/">Home</NavLink>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `font-medium text-[16px] p-0  ${
+                    isActive
+                      ? "bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent border-b-2 border-b-purple-500 rounded-none "
+                      : "text-slate-950"
+                  }`
+                }
+              >
+                Home
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/apps">Apps</NavLink>
+              <NavLink
+                to="/apps"
+                className={({ isActive }) =>
+                  `font-medium text-[16px] p-0  ${
+                    isActive
+                      ? "bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent border-b-2 border-b-purple-500 rounded-none "
+                      : "text-slate-950"
+                  }`
+                }
+              >
+                Apps
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/install">Installation</NavLink>
+              <NavLink
+                to="/install"
+                className={({ isActive }) =>
+                  `font-medium text-[16px] p-0  ${
+                    isActive
+                      ? "bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent border-b-2 border-b-purple-500 rounded-none "
+                      : "text-slate-950"
+                  }`
+                }
+              >
+                Installation
+              </NavLink>
             </li>
           </ul>
         </div>
