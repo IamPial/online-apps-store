cfa0bfc57097742f24a1c5a4c2bbdc10426e8fb8
Pial <pialname394@gmail.com>
2026-04-09 02:20:47 +0600

    create app found error page

---
+++ src/pages/NotFoundPage/AppFoundErrorPage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router";
+import errorImg from "../../assets/images/App-Error.png";
+
+const AppFoundErrorPage = () => {
+  return (
+    <div className="container mx-auto py-20">
+      <img src={errorImg} alt="Error" className="mx-auto mb-7" />
+      <div className="flex justify-center items-center flex-col space-y-5">
+        <h2 className="font-semibold text-5xl">OPPS!! APP NOT FOUND</h2>
+        <p className="text-xl text-gray-600">
+          The App you are requesting is not found on our system. please try
+          another apps
+        </p>
+        <Link
+          to="/apps"
+          className="btn btn-lg text-white bg-linear-to-r from-[#632EE3] to-[#9F62F2]"
+        >
+          Go Back!
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default AppFoundErrorPage;
