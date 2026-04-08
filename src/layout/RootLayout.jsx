c0ba21ce95d6dbde9b94466990f229cbcf213517
Pial <pialname394@gmail.com>
2026-04-08 15:34:10 +0600

    added footer section here

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -3,12 +3,14 @@ import HomePage from "../pages/homepage/HomePage";
 
 import { Outlet } from "react-router";
 import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
 
 const RootLayout = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
