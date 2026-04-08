6ac4ce1f5bd02d341814fcda915407b6898d9cf2
Pial <pialname394@gmail.com>
2026-04-09 01:45:44 +0600

    added search functionality

--- src/pages/apps/Apps.jsx
+++ src/pages/apps/Apps.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router";
 import { FaStar } from "react-icons/fa";
 import { HiOutlineDownload } from "react-icons/hi";
 const Apps = () => {
   const data = useLoaderData();
-  console.log(data);
+  const [searchItem, setSearchItem] = useState("");
 
+  const search = data.filter((searchingData) =>
+    searchingData.name.toLowerCase().includes(searchItem.toLowerCase()),
+  );
+  const allData = search.length > 0 ? search : data;
+  const errorMessage = searchItem.length > 0 && search.length === 0;
   return (
     <div>
       <div className="container mx-auto py-20">
@@ -34,12 +39,57 @@ const Apps = () => {
                 <path d="m21 21-4.3-4.3"></path>
               </g>
             </svg>
-            <input type="search" placeholder="Search" />
+            <input
+              onChange={(e) => setSearchItem(e.target.value)}
+              type="search"
+              placeholder="Search"
+            />
           </label>
         </div>
 
-        <div className="grid grid-cols-4 gap-4">
-          {data.map((cardData, index) => {
+        <div>
+          {errorMessage ? (
+            <p>Heelo Me</p>
+          ) : (
+            <div className="grid grid-cols-4 gap-4">
+              {allData.map((cardData, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="card bg-base-100 p-4 shadow-none hover:shadow-2xl transition-all duration-300 scale-95 hover:scale-100"
+                  >
+                    <figure className="mb-4">
+                      <img
+                        className="h-60 rounded-lg"
+                        src={cardData.icon}
+                        alt="Shoes"
+                      />
+                    </figure>
+                    <div className="card-body p-0 space-y-4">
+                      <h2 className="card-title">{cardData.name}</h2>
+                      <div className="flex justify-between items-center gap-30">
+                        <p className="bg-[#f1f5e8] flex justify-evenly items-center p-2 rounded-lg ">
+                          <HiOutlineDownload className="text-[16px] text-[#00D390] " />
+                          <span className="text-[16px] font-medium  text-[#00D390]">
+                            {cardData.stats.downloads}
+                          </span>
+                        </p>
+                        <p className="bg-[#fff0e1] flex justify-evenly items-center p-2  rounded-lg">
+                          <FaStar className="text-[#FF8811] " />
+                          <span className="text-[16px] font-medium  text-[#FF8811]">
+                            {cardData.rating}
+                          </span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+        {/* <div className="grid grid-cols-4 gap-4">
+          {allData.map((cardData, index) => {
             return (
               <div
                 key={index}
@@ -72,7 +122,7 @@ const Apps = () => {
               </div>
             );
           })}
-        </div>
+        </div> */}
       </div>
     </div>
   );
