68e0e2ae6c25e6ff5925c64fc633c312952d4f9a
Pial <pialname394@gmail.com>
2026-04-10 16:13:26 +0600

    create trending page for show trending apps

--- src/components/pages/trendingPage/TrendingPage.jsx
+++ src/components/pages/trendingPage/TrendingPage.jsx
@@ -1,23 +1,21 @@
 import React, { use } from "react";
-import Card from "../../../ui/Card";
 import { Link } from "react-router";
+import HomeCard from "../../../ui/homeCard";
 
-const fetchData = fetch("/home.json").then((res) => res.json());
-
-const TrendingPage = () => {
+const TrendingPage = ({ fetchData }) => {
   const data = use(fetchData);
-  console.log(data);
+
   return (
     <div className="container mx-auto py-20">
-      <div className="space-y-4 flex justify-center flex-col items-center mb-10">
+      <div className="space-y-4 flex justify-center flex-col items-center pb-10">
         <h2 className="text-5xl font-bold ">Trending Apps</h2>
         <p className="text-center text-md md:text-xl text-gray-600">
           Explore All Trending Apps on the Market developed by us
         </p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-4 md:px-0">
-        {data.map((cardData) => {
-          return <Card cardData={cardData} key={cardData} />;
+        {data.map((homeCard, index) => {
+          return <HomeCard cardData={homeCard} key={index} />;
         })}
       </div>
       <div className="flex justify-center items-center pt-20">
