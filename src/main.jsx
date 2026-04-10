e456b1b8e487ea934cdcd4713a827441e6ffdc78
Pial <pialname394@gmail.com>
2026-04-10 22:48:10 +0600

    added toastContainer to this main components

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { router } from "./routes/Routes";
 import { RouterProvider } from "react-router/dom";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer autoClose={3000} />
   </StrictMode>,
 );
