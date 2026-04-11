081bf3d5bca9cb385ae90a582bc8f579e864da4d
Pial <pialname394@gmail.com>
2026-04-12 03:32:18 +0600

    remove the toastContainer from this components

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { router } from "./routes/Routes";
 import { RouterProvider } from "react-router/dom";
-import { ToastContainer } from "react-toastify";
+
 import AppContext from "./context/AppContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppContext>
       <RouterProvider router={router} />
-      <ToastContainer autoClose={3000} />
     </AppContext>
   </StrictMode>,
 );
