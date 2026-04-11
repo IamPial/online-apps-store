04631f4d0dbdf39102d9b00203843c43232da38a
Pial <pialname394@gmail.com>
2026-04-12 00:41:04 +0600

    refactor homepage with more readable code

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,25 +1,23 @@
 import React, { Suspense } from "react";
 
-import TrendingPage from "../../components/pages/trendingPage/trendingPage";
 import Banner from "../../components/homepage/Banner";
 import StatsSection from "../../components/homepage/StatsSection";
-
-const fetchData = fetch("/home.json").then((res) => res.json());
+import TrendingPage from "../../components/homepage/TrendingPage";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
       <StatsSection />
-      <Suspense
+      {/* <Suspense
         fallback={
           <div className="py-20 flex justify-center items-center">
             <span className="loading loading-bars loading-xl"></span>
           </div>
         }
       >
-        <TrendingPage fetchData={fetchData}></TrendingPage>
-      </Suspense>
+      </Suspense> */}
+      <TrendingPage></TrendingPage>
     </div>
   );
 };
