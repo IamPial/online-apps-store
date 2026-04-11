32b515e7cc63b5852a3b4c02b90e9e85e7fc2fcc
Pial <pialname394@gmail.com>
2026-04-12 02:01:20 +0600

    remove the loader from the Apps components

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -19,7 +19,6 @@ export const router = createBrowserRouter([
       {
         path: "/apps",
         element: <Apps />,
-        loader: () => fetch("/apps.json"),
       },
       {
         path: "/apps/:id",
