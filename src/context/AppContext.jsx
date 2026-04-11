3c4a8f0d4e515dd4e3154f9fe7c6e5cd9e8c5b3c
Pial <pialname394@gmail.com>
2026-04-12 04:05:09 +0600

    create context components

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,20 +1,3 @@
-import React, { createContext, useState } from "react";
+import React, { createContext } from "react";
 
 export const CreateAppContext = createContext();
-
-const AppContext = ({ children }) => {
-  const [installedApps, setInstalledApps] = useState([]);
-  const data = {
-    installedApps,
-    setInstalledApps,
-  };
-  return (
-    <div>
-      <CreateAppContext.Provider value={data}>
-        {children}
-      </CreateAppContext.Provider>
-    </div>
-  );
-};
-
-export default AppContext;
