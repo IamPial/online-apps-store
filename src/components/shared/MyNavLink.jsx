c3efca0e18a7438c8ae2f970a4bfe34f4efb0467
Pial <pialname394@gmail.com>
2026-04-11 23:21:55 +0600

    pass a className props for styling small screen

--- src/components/shared/MyNavLink.jsx
+++ src/components/shared/MyNavLink.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router";
 
-const MyNavLink = ({ to, children }) => {
+const MyNavLink = ({ to, children, className }) => {
   return (
     <NavLink
       to={to}
       className={({ isActive }) =>
         `font-medium text-[16px] p-0 ${
           isActive
-            ? "bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent border-b-2  border-b-purple-500 rounded-none "
+            ? `bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent  border-b-2  border-b-purple-500 rounded-none ${className}`
             : "text-slate-950"
         }`
       }
