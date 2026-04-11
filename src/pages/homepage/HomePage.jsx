da70ea6315e3bdb2e99fe9e2a90fe07aa6f0708b
Pial <pialname394@gmail.com>
2026-04-12 02:03:15 +0600

    remove the suspense import from react

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 
 import Banner from "../../components/homepage/Banner";
 import StatsSection from "../../components/homepage/StatsSection";
