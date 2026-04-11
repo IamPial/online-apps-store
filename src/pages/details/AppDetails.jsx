e3978438446b6cc01cdab84f4d7b3be631b59750
Pial <pialname394@gmail.com>
2026-04-12 03:33:23 +0600

    use context for globallay sharing this details of app data

--- src/pages/details/AppDetails.jsx
+++ src/pages/details/AppDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { HiOutlineDownload } from "react-icons/hi";
 import { FaStar } from "react-icons/fa";
 import { BiSolidLike } from "react-icons/bi";
@@ -7,10 +7,12 @@ import { Link, useParams } from "react-router";
 import { toast } from "react-toastify";
 import useApps from "../../hooks/useApps";
 import { RingLoader } from "react-spinners";
+import { CreateAppContext } from "../../context/AppContext";
 
 const AppDetails = () => {
   const { id } = useParams();
   const { apps, loading } = useApps();
+  const { installedApps, setInstalledApps } = useContext(CreateAppContext);
   const [btnIsSelect, setBtnIsSelect] = useState(false);
 
   //for handling undefined data with loading spinners
@@ -23,8 +25,8 @@ const AppDetails = () => {
   }
 
   //generating bar chartData
-  const obj = apps.find((item) => item.id === parseInt(id));
-  const barChartData = Object.entries(obj.ratings_breakdown).map(
+  const expectedApp = apps.find((item) => item.id === parseInt(id));
+  const barChartData = Object.entries(expectedApp.ratings_breakdown).map(
     ([key, value]) => ({
       star: key.replace("_star", " star"),
       count: value,
@@ -32,31 +34,38 @@ const AppDetails = () => {
   );
 
   const handleInstallButton = () => {
-    const newData = JSON.parse(localStorage.getItem("selectItem")) || [];
-    const isExistingApps = newData.find((item) => item.id === obj.id);
+    const isExistingApps = installedApps.find(
+      (item) => item.id === expectedApp.id,
+    );
     if (isExistingApps) {
-      toast.error(`${obj.name} Already Installed`);
+      toast.error(`${expectedApp.name} Already Installed`);
       return;
     }
-
-    const newValue = [...newData, obj];
-    localStorage.setItem("selectItem", JSON.stringify(newValue));
-    toast.success(`${obj.name} Installed Successfully`);
+    setInstalledApps([...installedApps, expectedApp]);
+    toast.success(`${expectedApp.name} Installed Successfully`);
     setBtnIsSelect(true);
   };
+
+  console.log("installed apps", installedApps);
 
   return (
     <div>
       <div className="container mx-auto px-2 md:px-0">
         <div className="pt-20 pb-10 flex flex-col justify-center items-center lg:justify-normal lg:items-start lg:flex-row lg:px-0 px-4 gap-10">
-          <img src={obj.icon} alt="demo" className="w-100 rounded-xl h-80" />
+          <img
+            src={expectedApp.icon}
+            alt="demo"
+            className="w-100 rounded-xl h-80"
+          />
           <div className="flex-1">
             <div className="space-y-2">
-              <h2 className="text-3xl font-bold text-slate-900">{obj.name}</h2>
+              <h2 className="text-3xl font-bold text-slate-900">
+                {expectedApp.name}
+              </h2>
               <p className="text-xl text-gray-600">
                 Developed by{" "}
                 <span className="font-semibold text-[#632EE3]">
-                  {obj.developer}
+                  {expectedApp.developer}
                 </span>{" "}
               </p>
             </div>
@@ -67,7 +76,7 @@ const AppDetails = () => {
                   <HiOutlineDownload className="text-3xl text-[#00D390] " />
                   <span className="text-[16px] text-gray-500">Downloads</span>
                   <span className="font-extrabold text-[40px] text-slate-900">
-                    {obj.stats.downloads}
+                    {expectedApp.stats.downloads}
                   </span>
                 </div>
                 <div className="flex flex-col space-y-2">
@@ -76,7 +85,7 @@ const AppDetails = () => {
                     Average Ratings
                   </span>
                   <span className="font-extrabold text-[40px] text-slate-900">
-                    {obj.rating}
+                    {expectedApp.rating}
                   </span>
                 </div>
                 <div className="flex flex-col space-y-2">
@@ -85,7 +94,7 @@ const AppDetails = () => {
                     Total Reviews
                   </span>
                   <span className="font-extrabold text-[40px] text-slate-900">
-                    {obj.stats.total_reviews}
+                    {expectedApp.stats.total_reviews}
                   </span>
                 </div>
               </div>
@@ -96,7 +105,7 @@ const AppDetails = () => {
               >
                 {btnIsSelect
                   ? "Installed"
-                  : `${obj.install_label} (${obj.size_mb}) MB`}
+                  : `${expectedApp.install_label} (${expectedApp.size_mb}) MB`}
               </button>
             </div>
           </div>
@@ -107,7 +116,9 @@ const AppDetails = () => {
         <div className="divider h-0.5 bg-gray-300"></div>
         <div className="pt-10 pb-20 space-y-6">
           <h2 className="text-2xl font-semibold text-slate-950">Description</h2>
-          <p className="text-lg text-gray-600 leading-8 ">{obj.description}</p>
+          <p className="text-lg text-gray-600 leading-8 ">
+            {expectedApp.description}
+          </p>
         </div>
 
         <div className="flex justify-center items-center py-2">
