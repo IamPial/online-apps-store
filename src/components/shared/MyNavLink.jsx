c10959befa4f7a942fccb8a2031d51ea5f5925bf
Pial <pialname394@gmail.com>
2026-04-11 23:09:55 +0600

    created MyNavLink components for reusable components

---
+++ src/components/shared/MyNavLink.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router";
+
+const MyNavLink = ({ to, children }) => {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        `font-medium text-[16px] p-0 ${
+          isActive
+            ? "bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent border-b-2  border-b-purple-500 rounded-none "
+            : "text-slate-950"
+        }`
+      }
+    >
+      {children}
+    </NavLink>
+  );
+};
+
+export default MyNavLink;
